1919d9382b7a50f868c76d1b3b4c9d8ddbf883f2
brian <brianrlam@gmail.com>
2023-08-06 09:32:21 -0400

    change follows forign key

--- app/models/user.py
+++ app/models/user.py
@@ -17,8 +17,8 @@ class User(db.Model, UserMixin):
     picture = db.Column(db.String(255))
 
     follows = db.Table('follows',
-        db.Column('follower_id', db.Integer, db.ForeignKey('users.id'), primary_key=True),
-        db.Column('followee_id', db.Integer, db.ForeignKey('users.id'), primary_key=True)
+        db.Column('follower_id', db.Integer, db.ForeignKey(id), primary_key=True),
+        db.Column('followee_id', db.Integer, db.ForeignKey(id), primary_key=True)
     )
 
     followers = db.relationship('User', secondary='follows',
